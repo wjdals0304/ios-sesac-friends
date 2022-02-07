f44ca9331f7d5c3f4d34c9651a6f261dd7dca7ea
jmkim <wjdals0304@gmail.com>
2022-02-07 22:53:31 +0900

    add:  HobbyCollectionViewCell 파일 추가

---
+++ SeSACFriends/Presentation/Hobby/HobbyCollectionViewCell.swift
@@ -0,0 +1,65 @@
+//
+//  HobbyCollectionView.swift
+//  SeSACFriends
+//
+//  Created by 김정민 on 2022/02/06.
+//
+
+import Foundation
+import UIKit
+
+class HobbyCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "HobbyCollectionViewCell"
+    
+    
+    let textLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.getRegularFont(.regular_12)
+        label.textColor = UIColor.getColor(.defaultTextColor)
+        return label
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setup(with text: String) {
+        
+        [
+         textLabel
+        ].forEach { addSubview($0) }
+        
+        textLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(self.snp.centerX)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+        
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        
+        textLabel.text = text
+        self.contentView.isUserInteractionEnabled  = true 
+    }
+    
+    func setupMyHobby(with text : String) {
+        
+        [
+         textLabel
+        ].forEach { addSubview($0) }
+        
+        textLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(self.snp.centerX)
+            make.centerY.equalTo(self.snp.centerY)
+        }
+        
+        self.layer.borderWidth = 1
+        self.layer.cornerRadius = 8
+        self.layer.borderColor = UIColor.getColor(.activeColor).cgColor
+        textLabel.textColor = UIColor.getColor(.activeColor)
+        
+        textLabel.addTrailing(image: UIImage(named: "close_small") ?? UIImage(), with: text)
+    }
+    
+        
+}
