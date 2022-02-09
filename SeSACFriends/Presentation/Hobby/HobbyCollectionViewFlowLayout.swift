3b35e8df1a447a4f1b351b6e4f869130da2793a6
jmkim <wjdals0304@gmail.com>
2022-02-10 00:55:40 +0900

    del: print 문 제거

--- SeSACFriends/Presentation/Hobby/HobbyCollectionViewFlowLayout.swift
+++ SeSACFriends/Presentation/Hobby/HobbyCollectionViewFlowLayout.swift
@@ -11,25 +11,37 @@ import UIKit
 class HobbyCollectionViewFlowLayout : UICollectionViewFlowLayout {
     
     var tempCellAttributesArray = [UICollectionViewLayoutAttributes]()
-    let leftEdgeInset: CGFloat = 10
+    let leftEdgeInset: CGFloat = 0
     
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
         let cellAttributesArray = super.layoutAttributesForElements(in: rect)
-        //Oth position cellAttr is InConvience Emoji Cell, from 1st onwards info cells are there, thats why we start count from 2nd position.
+    
+        
+
         if(cellAttributesArray != nil && cellAttributesArray!.count > 1) {
+            
             for i in 1..<(cellAttributesArray!.count) {
+                
                 let prevLayoutAttributes: UICollectionViewLayoutAttributes = cellAttributesArray![i - 1]
+                
                 let currentLayoutAttributes: UICollectionViewLayoutAttributes = cellAttributesArray![i]
+                
                 let maximumSpacing: CGFloat = 8
+                
                 let prevCellMaxX: CGFloat = prevLayoutAttributes.frame.maxX
                 //UIEdgeInset 30 from left
-                let collectionViewSectionWidth = self.collectionViewContentSize.width - leftEdgeInset
+                let collectionViewSectionWidth = self.collectionViewContentSize.width
+                - leftEdgeInset
+                
                 let currentCellExpectedMaxX = prevCellMaxX + maximumSpacing + (currentLayoutAttributes.frame.size.width )
+                
                 if currentCellExpectedMaxX < collectionViewSectionWidth {
                     var frame: CGRect? = currentLayoutAttributes.frame
                     frame?.origin.x = prevCellMaxX + maximumSpacing
                     frame?.origin.y = prevLayoutAttributes.frame.origin.y
                     currentLayoutAttributes.frame = frame ?? CGRect.zero
+                    
                 } else {
 //                     self.shiftCellsToCenter()
                     currentLayoutAttributes.frame.origin.x = leftEdgeInset
@@ -38,11 +50,10 @@ class HobbyCollectionViewFlowLayout : UICollectionViewFlowLayout {
                         currentLayoutAttributes.frame.origin.y = prevLayoutAttributes.frame.origin.y + prevLayoutAttributes.frame.size.height + 08
                     }
                 }
-                // print(currentLayoutAttributes.frame)
+
             }
-            //print("Main For Loop End")
         }
-//         self.shiftCellsToCenter()
+
         return cellAttributesArray
     }
     
