b3bb1e8e8aa3528bb98acfc7731b230305b5d826
jmkim <wjdals0304@gmail.com>
2022-02-14 22:23:52 +0900

    add:  api 상태코드 case 추가

--- SeSACFriends/Entities/APIStatus.swift
+++ SeSACFriends/Entities/APIStatus.swift
@@ -20,5 +20,13 @@ enum APIStatus   {
     case unregisterdUser
     case registerdUser
     case banNick
-    
+    case reportThreeUser // 신고하기 3번 이상 받은 유저
+    case penaltyone // 취미 함께하기 약속 취소 패널티 1단계
+    case penaltytwo // 패널티 2단계
+    case penaltyThree // 패널티 3단계
+    case noGender // 내 성별이 설정되지 않음
+    case alreadyRequest // 상대가 이미 나에게 취미 함께하기 요청한 상태
+    case suspendRequest // 상대가 취미 함께할 친구 찾기 중단한 상태
+    case matchedState // 내가 이미 다른 사람과 취미 함께하기 매칭된 상태
+    case noChat // 채팅전송불가 
 }
