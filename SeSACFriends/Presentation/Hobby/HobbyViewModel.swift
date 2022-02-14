413450acb5be350cc5a65a08541665cf70c217f1
jmkim <wjdals0304@gmail.com>
2022-02-14 14:41:15 +0900

    mod : postOnqueue API   homeViewModel ->  hobbyViewModel 로 이동

--- SeSACFriends/Presentation/Hobby/HobbyViewModel.swift
+++ SeSACFriends/Presentation/Hobby/HobbyViewModel.swift
@@ -10,6 +10,52 @@ import Foundation
 
 class HobbyViewModel {
     
+    
+    
+    
+    func postOnqueue(region : Int, lat: Double, long: Double , completion: @escaping(Queue?, APIStatus?) -> Void) {
+        
+        let idtoken = UserManager.idtoken!
+        let queueNetwork = QueueNetwork(idtoken: idtoken)
+        
+        
+        queueNetwork.postOnqueue(region: region, lat: lat, long: long) { queue, APIStatus in
+            
+            switch APIStatus {
+                
+             case .success:
+                completion(queue,.success)
+                
+            case .expiredToken :
+                completion(nil,.expiredToken)
+            
+                    
+            case .unregisterdUser:
+                print("unregisterdUser")
+                completion(nil,.failed)
+
+            case .clientError :
+                print("clientError")
+                completion(nil,.failed)
+                
+            case .serverError :
+                print("serverError")
+                completion(nil,.failed)
+                
+            case .failed :
+                completion(nil,.failed)
+
+            default :
+                completion(nil,.failed)
+            
+            }
+            
+        }
+        
+    }
+    
+    
+    
     func postQueue(region: Int, lat: Double , long: Double , completion: @escaping(APIStatus?) -> Void ){
         
         let idtoken = UserManager.idtoken!
