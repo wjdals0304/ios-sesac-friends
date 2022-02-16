6ff6b29f332212f539abf131450adbd1a448725c
jmkim <wjdals0304@gmail.com>
2022-02-16 17:51:28 +0900

    mod: UIviewcontroller -> viewModel  함수 리팩토링

--- SeSACFriends/Presentation/SignUp/SignUpViewModel.swift
+++ SeSACFriends/Presentation/SignUp/SignUpViewModel.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class SignUpViewModel {
     
+    /// 이메일 검증
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluate(with: testStr)
+    }
     
     /// 회원가입
     /// - Parameters:
