03756509a10732cd5e46a88e7e097a591639fd68
jmkim <wjdals0304@gmail.com>
2022-02-16 14:59:17 +0900

    add: file add

---
+++ SeSACFriends/Presentation/Base/BaseUIView.swift
@@ -0,0 +1,35 @@
+//
+//  BaseUIView.swift
+//  SeSACFriends
+//
+//  Created by 김정민 on 2022/02/16.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+
+class BaseUIView : UIView {
+    
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        setup()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setup() {
+        
+    }
+    
+    func setupConstraint(){
+        
+    }
+    
+}
---
+++ SeSACFriends/Presentation/Base/BaseViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BaseViewController.swift
+//  SeSACFriends
+//
+//  Created by 김정민 on 2022/02/16.
+//
+
+import Foundation
+import UIKit
+
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        setupConstraint()
+        addTarget()
+        setupNavigationBar()
+        view.backgroundColor = .systemBackground
+    }
+    
+    func setup(){
+        
+    }
+    
+    func setupConstraint(){
+        
+    }
+    
+    func addTarget() {
+        
+    }
+    
+    func setupNavigationBar() {
+        
+        
+    }
+    
+}
