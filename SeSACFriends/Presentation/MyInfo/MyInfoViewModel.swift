12c957ae56d95da329a595be1994822de2981c18
jmkim <wjdals0304@gmail.com>
2022-02-16 22:25:05 +0900

    add: get user api 추가

--- SeSACFriends/Presentation/MyInfo/MyInfoViewModel.swift
+++ SeSACFriends/Presentation/MyInfo/MyInfoViewModel.swift
@@ -10,6 +10,74 @@ import Foundation
 
 class MyInfoViewModel {
     
+    let imageArray = ["notice","faq","qna","setting_alarm","permit"]
+    let textArray = ["공지사항","자주 묻는 질문","1:1 문의","알람 설정","이용 약관" ]
+    
+    
+    ///  내 유저 정보가져오기
+    /// - Parameters:
+    ///   - idtoken: 파이어베이스 id 토큰
+    ///   - completion:  user, apistatus  리턴
+    func getUser(idtoken: String , completion: @escaping(User?,APIStatus?) -> Void ) {
+     
+        let userNetwork = UserNetwork(idtoken: idtoken)
+        
+        userNetwork.getUser { User, APIStatus in
+            
+            switch APIStatus {
+                
+            case .success:
+                completion(User,.success)
+                
+            case .unregisterdUser:
+                completion(nil,.unregisterdUser)
+                
+            case .noData:
+                print(#function)
+                print(".noData")
+                completion(nil,.failed)
+                
+            case .invalidData:
+                print(#function)
+                print(".invalidData")
+                completion(nil,.failed)
+                
+            case .expiredToken:
+                print(#function)
+                print(".expiredToken")
+                completion(nil,.expiredToken)
+    
+                
+            case .clientError :
+                print(#function)
+                print(".clientError")
+                completion(nil,.failed)
+            
+            case .serverError :
+                print(#function)
+                print(".serverError")
+                completion(nil,.failed)
+                
+            case  .none, .failed:
+                print(#function)
+                print(".failed")
+                completion(nil,.failed)
+                
+            default :
+                print(#function)
+                print("default")
+                completion(nil,.failed)
+
+                
+            }
+                    
+        }
+        
+        
+        
+    }
+    
+    
     
     func withDrawUser(completion: @escaping(APIStatus?) -> Void) {
         
