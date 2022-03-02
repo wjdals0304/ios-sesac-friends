e4c5ec02dfc821e103d03522ee84c2d15d690d5a
jmkim <wjdals0304@gmail.com>
2022-03-02 17:35:39 +0900

    add: cell 클릭시 미리보기 이미지 변경 로직 추가

--- SeSACFriends/Presentation/SesacShop/SesacShopViewController.swift
+++ SeSACFriends/Presentation/SesacShop/SesacShopViewController.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 import UIKit
-import Tabman
-import Pageboy
 import SnapKit
 
-final class SesacShopViewController : TabmanViewController   {
+final class SesacShopViewController: UIViewController {
     
-    let segmentedControl : UISegmentedControl = {
+    let sesacShopViewModel = SesacShopViewModel()
+    
+    private lazy var segmentedControl: UISegmentedControl = {
         let items = ["새싹", "배경"]
         let segmentedControl = UISegmentedControl(items: items)
         segmentedControl.selectedSegmentIndex = 0
@@ -21,40 +21,49 @@ final class SesacShopViewController : TabmanViewController   {
         return segmentedControl
     }()
     
-    let imageUIView : UIView = {
+    private let imageUIView: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
         return view
     }()
     
-    let contentView : UIView = {
+    private let sesacUIImageView  = UIImageView()
+    private let sesacBackgrounImageUIView = UIImageView()
+    
+    private let contentView: UIView = {
         let view = UIView()
         view.backgroundColor = .systemBackground
 
         return view
     }()
     
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("저장하기", for: .normal)
+        button.titleLabel?.textColor = UIColor.getColor(.whiteTextColor)
+        button.backgroundColor = UIColor.getColor(.activeColor)
+        button.titleLabel?.font = UIFont.getRegularFont(.regular_14)
+        button.addTarget(self, action: #selector(saveButtonClicked), for: .touchUpInside)
+        button.layer.cornerRadius = 8
+        return button
+    }()
     
     let sesacImageView = SesacImageView()
     let sesacBackgroundView = SesacBackgroundView()
     
     override func viewDidLoad() {
         
-        
+        getShopMyinfo()
         setup()
         setupConstraint()
         
         self.contentView.bringSubviewToFront(self.sesacImageView)
     }
     
-    
-    func setup(){
+    func setup() {
         
         view.backgroundColor = .systemBackground
 
         title = "새싹샵"
-        self.tabBarController?.tabBar.isHidden = true
-        
         
         [
             imageUIView,
@@ -65,8 +74,19 @@ final class SesacShopViewController : TabmanViewController   {
         [
            sesacImageView,
            sesacBackgroundView
-        ].forEach{ contentView.addSubview($0) }
+        ].forEach { contentView.addSubview($0) }
         
+        [
+            sesacUIImageView,
+            sesacBackgrounImageUIView,
+            saveButton
+        ].forEach {  imageUIView.addSubview($0) }
+        
+        self.imageUIView.bringSubviewToFront(sesacUIImageView)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(chagneSesacImage), name: .changeSesacImage, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(chagneBackgroundImage), name: .changeBackgroundImage, object: nil)
     }
     
     func setupConstraint() {
@@ -86,23 +106,37 @@ final class SesacShopViewController : TabmanViewController   {
         
         contentView.snp.makeConstraints { make in
             make.top.equalTo(segmentedControl.snp.bottom)
-            make.leading.trailing.bottom.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
         
-        sesacImageView.snp.makeConstraints { make in
+        sesacImageView.snp.makeConstraints { make in 
             make.edges.equalToSuperview()
         }
         
         sesacBackgroundView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        
+        sesacBackgrounImageUIView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        sesacUIImageView.snp.makeConstraints { make in
+            make.top.equalTo(sesacBackgrounImageUIView.snp.top).offset(11)
+            make.leading.equalTo(sesacBackgrounImageUIView.snp.leading).offset(75)
+            make.width.equalTo(sesacBackgrounImageUIView.snp.width).multipliedBy(0.53)
+            make.height.equalTo(sesacBackgrounImageUIView.snp.width).multipliedBy(0.53)
+        }
+        saveButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(13)
+            make.trailing.equalToSuperview().inset(15)
+            make.height.equalTo(40)
+            make.width.equalTo(sesacBackgrounImageUIView.snp.width).multipliedBy(0.22)
+        }
     
     }
     
-    
-  
 }
-
 
 private extension SesacShopViewController {
     
@@ -111,16 +145,99 @@ private extension SesacShopViewController {
         switch sender.selectedSegmentIndex {
         case 0 :
             self.contentView.bringSubviewToFront(sesacImageView)
-            break
         case 1:
             self.contentView.bringSubviewToFront(sesacBackgroundView)
-
-            break
         default :
             break
         }
     }
     
+    func getShopMyinfo() {
+        
+        sesacShopViewModel.getShopMyinfo { APIStatus in
+
+            switch APIStatus {
+                
+            case .success:
+                
+                let sesacImage = self.sesacShopViewModel.setSesacImage(index: self.sesacShopViewModel.sesac)
+                let sesacBackgroundImage = self.sesacShopViewModel.setBackground(index: self.sesacShopViewModel.background)
+                
+                self.sesacUIImageView.image = UIImage(named: sesacImage)
+                self.sesacBackgrounImageUIView.image = UIImage(named: sesacBackgroundImage)
+                
+            case .expiredToken:
+                AuthNetwork.getIdToken { error in
+                    switch error {
+                    case .success :
+                           self.getShopMyinfo()
+                    case .failed :
+                           self.view.makeToast(APIErrorMessage.failed.rawValue)
+                    default :
+                            self.view.makeToast(APIErrorMessage.failed.rawValue)
+                        }
+                    }
+            case .failed :
+                self.view.makeToast(APIErrorMessage.failed.rawValue)
+                
+            default :
+                self.view.makeToast(APIErrorMessage.failed.rawValue)
+                
+            }
+        }
+    }
     
+    @objc func chagneSesacImage(_ notification: Notification) {
     
+        let imageIndex = notification.object as! Int
+        
+        self.sesacShopViewModel.sesac = imageIndex
+        
+        let sesacImage = self.sesacShopViewModel.setSesacImage(index: imageIndex)
+
+        self.sesacUIImageView.image = UIImage(named: sesacImage)
+
+    }
+    
+    @objc func chagneBackgroundImage(_ notification: Notification) {
+        
+        let imageIndex = notification.object as! Int
+        
+        self.sesacShopViewModel.background = imageIndex
+        
+        let sesacBackgroundImage = self.sesacShopViewModel.setBackground(index: imageIndex)
+
+        self.sesacBackgrounImageUIView.image = UIImage(named: sesacBackgroundImage)
+
+    }
+    
+    @objc func saveButtonClicked() {
+        
+        sesacShopViewModel.updateShop(sesac: self.sesacShopViewModel.sesac, background: self.sesacShopViewModel.background) { APIStatus in
+            
+            switch APIStatus {
+                
+            case .success :
+                self.view.makeToast("성공적으로 저장되었습니다.")
+            case .unownItem:
+                self.view.makeToast("구매가 필요한 아이템이 있어요.")
+            case .expiredToken:
+                AuthNetwork.getIdToken { error in
+                    switch error {
+                    case .success :
+                           self.getShopMyinfo()
+                    case .failed :
+                           self.view.makeToast(APIErrorMessage.failed.rawValue)
+                    default :
+                            self.view.makeToast(APIErrorMessage.failed.rawValue)
+                        }
+                    }
+                
+            default :
+                self.view.makeToast(APIErrorMessage.failed.rawValue)
+                
+            }
+            
+        }
+    }
 }
